4dd7af6252deb6401de89117625a422d781064da
Shawn <shawn@shawngrover.ca>
2025-08-08 16:07:41 -0600

    feat: Expose SaasManager in __init__.py
    Adds `from .manager import SaasManager` to `src/saas_foundation/__init__.py` to allow direct import of the `SaasManager` facade from the top-level `saas_foundation` package.

--- src/saas_foundation/__init__.py
+++ src/saas_foundation/__init__.py
@@ -5,3 +5,4 @@ from .logging_system.manager import LogManager
 from .multi_tenant.manager import MultiTenantManager
 from .payment_gateway.manager import PaymentGatewayManager
 from .subscription.manager import SubscriptionManager
+from .manager import SaasManager
